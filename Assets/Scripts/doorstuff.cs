cdd42215314ed53c3fad7863e2f84985fa7b28f3
Isaac <isacmen@gmail.com>
2017-06-12 16:17:50 -0700

    Locks and Keys

--- Assets/Scripts/doorstuff.cs
+++ Assets/Scripts/doorstuff.cs
@@ -6,24 +6,35 @@ public class doorstuff : MonoBehaviour {
 
 	private AudioSource src;
 	private bool opened;
+	private int timer;
 
 	// Use this for initialization
 	void Start () {
 		src = gameObject.GetComponent<AudioSource> (); 
 		opened = false;
+		timer = 0;
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		if (opened) {
+			timer++;
+		}
+		if (timer > 60) {
+			GameObject.Destroy (this.gameObject);
+		}
 		
 	}
 
 	void OnTriggerEnter2D(Collider2D col)
 	{
 		if (col.CompareTag ("Player") && !opened) {
-			src.Play();
-			opened = true;
+			bool key = col.gameObject.GetComponent<keycode> ().haskey;
+			if (key) {
+				src.Play ();
+				opened = true;
+			}
 		}
 	}
 }
