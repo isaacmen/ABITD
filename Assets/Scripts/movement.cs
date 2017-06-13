27bf6c78cccad597cd8b3741c49c7f47434ac781
Isaac <isacmen@gmail.com>
2017-06-13 14:21:28 -0700

    Walk Animation and Death Room Bugfix

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -12,9 +12,12 @@ public class movement : MonoBehaviour {
     bool monster_check = false;
 	public bool walking = false;
 
+	private Animator anim;
+	private int Direction;
+
 	// Use this for initialization
 	void Start () {
-		
+		anim = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
@@ -24,26 +27,27 @@ public class movement : MonoBehaviour {
 
 		if (Input.GetKey (KeyCode.W) && can_move) {
 			transform.position = new Vector3 (pos.x, pos.y + speed, pos.z);
-			transform.eulerAngles = new Vector3 (0.0f, 0.0f, 0.0f);
+			anim.SetInteger ("Direction", 1);
 			walking = true;
 			//transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
 		} else if (Input.GetKey (KeyCode.S) && can_move) {
 			transform.position = new Vector3 (pos.x, pos.y - speed, pos.z);
-			transform.eulerAngles = new Vector3 (0.0f, 0.0f, 180.0f);
+			anim.SetInteger ("Direction", 3);
 			walking = true;
 			//transform.rotation = Quaternion.Euler(new Vector3(0, 0, 180));
 		} else if (Input.GetKey (KeyCode.A) && can_move) {
 			transform.position = new Vector3 (pos.x - speed, pos.y, pos.z);
-			transform.eulerAngles = new Vector3 (0.0f, 0.0f, 90.0f);
+			anim.SetInteger ("Direction", 4);
 			walking = true;
 			//transform.rotation = Quaternion.Euler(new Vector3(0, 0, 90));
 		} else if (Input.GetKey (KeyCode.D) && can_move) {
 			transform.position = new Vector3 (pos.x + speed, pos.y, pos.z);
-			transform.eulerAngles = new Vector3 (0.0f, 0.0f, 270.0f);
+			anim.SetInteger ("Direction", 2);
 			walking = true;
 			//transform.rotation = Quaternion.Euler(new Vector3(0, 0, 270));
 		} else {
 			walking = false;
+			anim.SetInteger ("Direction", 0);
 		}
 
         if (Input.GetKeyDown(KeyCode.R))
